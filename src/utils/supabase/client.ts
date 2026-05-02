a59592db73c1f14dc2861a15a96c5fafc3179d5b
rifai-norp <rifainwd@gmail.com>
2026-05-02 10:40:44 +0700

    init

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,15 @@
-import { createBrowserClient } from '@supabase/ssr'
+"use client";
+
+import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  // 🔥 validasi dulu
+  if (!url || !key) {
+    throw new Error("Supabase ENV tidak terbaca di client");
+  }
+
+  return createBrowserClient(url, key);
 }
